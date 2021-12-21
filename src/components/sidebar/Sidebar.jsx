905c5e8e6c54b855cdfdeff84cb53cdba6a93ca3
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-12-21 21:00:24 +0300

    Transfer of all components to 'components' file

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import "./sidebar.css";
+
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">
+                    ABOUT ME
+                </span>
+                <img src={require("../../img/profile.png")}
+                 alt="loading" />
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+            <div className="sidebarItem"></div>
+            <span className="sidebarTitle">CATEGORIES</span>
+            <ul className="sidebarList">
+                <li className="sidebarListItem">Music</li>
+                <li className="sidebarListItem">Sport</li>
+                <li className="sidebarListItem">Swim</li>
+                <li className="sidebarListItem">Cinema</li>
+            </ul>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                <i className="sidebarIcon fab fa-facebook-square"></i>
+                <i className="sidebarIcon fab fa-instagram-square"></i>
+                <i className="sidebarIcon fab fa-twitter-square"></i>
+                <i className="sidebarIcon fab fa-pinterest-square"></i>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
