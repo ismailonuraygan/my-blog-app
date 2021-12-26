624c2fe0381dc1fa874d631796cba943e668abb0
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-12-26 12:20:57 +0300

    Get all posts

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -24,25 +24,68 @@ router.post("/", async (req, res) => {
 //update post
 
 router.put("/:id", async (req, res) => {
-    if(req.body.postID === req.params.id) {
+    const post = await Post.findById(req.params.id)
+    if(req.params.id === req.body.postID || req.body.username === post.username) {
         try{
-            const updatedPost = await User.findByIdAndUpdate(req.params.id, {
+            const updatedPost = await Post.findByIdAndUpdate(req.params.id, {
                 title: req.body.title,
                 desc: req.body.desc,
                 categories: req.body.categories
             }, {new: true})
-            res.status(200).json(updatedPost);
-        } catch(err) {
+            res.status(200).json(updatedPost)
+        }catch(err) {
             res.status(500).json(err)
         }
-    } else {
-        res.status(401).json("you can update only your post")
+    }else {
+        res.status(401).json("You can update only your post")
     }
 })
 
+
 //delete post
+
+router.delete("/:id", async (req, res) => {
+    const post = await Post.findById(req.params.id)
+    if(req.params.id === req.body.postID || req.body.username === post.username){
+        try{
+            const deletedPost = await Post.findByIdAndDelete(req.params.id)
+            res.status(200).json("Post has been deleted!")
+        }catch(err) {
+            res.status(500).json(err)
+        }
+    }else {
+        res.status(401).json("You can delete only your post");
+    }
+})
 
 
 //get post
+router.get("/:id", async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+
+//get all posts
+
+router.get("/", async (req, res) => {
+    const userName = req.query.user;
+    const categoryName = req.query.category;
+    try{
+        let posts;
+        if(userName){
+            posts = await Post.find({username : userName})
+        } else {
+            posts = await Post.find();
+        }
+        res.status(200).json(posts)
+    }catch(err) {
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
