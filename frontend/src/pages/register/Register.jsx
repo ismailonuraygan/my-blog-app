75fe9cf95dab170c945bdbc806f6e7c31e717101
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-12-29 01:44:40 +0300

    Updating register page

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,6 +1,6 @@
 import "./register.css"
 import {Link} from "react-router-dom";
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import axios from 'axios';
 
 function Register() {
