181a81069b2136c90878276527140748597f6650
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-12-29 01:18:37 +0300

    Update index.js and install dependencies to update image

--- api/index.js
+++ api/index.js
@@ -5,20 +5,30 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const usersRoute = require("./routes/users");
 const postsRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const path = require("path")
+
 
 dotenv.config();
 app.use(express.json());
-
+app.use("/public", express.static(path.join(__dirname, "/public")));
 mongoose.connect(process.env.MONGO_URL ,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: true
 }).then(console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
+
+
+
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postsRoute);
+app.use("/api/categories", categoryRoute);
+
 
 
 app.listen("5000", () => console.log("backend is working!"));
